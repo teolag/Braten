811fd3df51eaac7aa09fd14cb9d2068e41620a9c
Teodor Lagerqvist <xio.git@teodor.se>
2015-10-04 19:36:27 +0200

    profile bugfix and styling

--- public_html/scripts/edit_board.php
+++ public_html/scripts/edit_board.php
@@ -1,13 +1,19 @@
-<?php 
+<?php
 require "../../includes/init.php";
 
 if(isset($_GET['do']) && $_GET['do']=="delete" && isset($_GET['id'])) {
 	$db->execute("DELETE FROM posts WHERE id=".intval($_GET['id'])." LIMIT 1");
 }
 elseif(empty($_POST['id'])) {
+
 	$db->insertArray("posts", $_POST['post']);
 
 	$notifyUsers = $db->getArray("SELECT id, email, firstName, lastName FROM users WHERE notifyBoard=1 AND email!='' && id!=".$user['id']);
+	//var_dump($_POST);
+	//var_dump($notifyUsers);
+	//exit;
+
+	// Testa skicka bara till mig
 	//$notifyUsers = $db->getArray("SELECT id, email, firstName, lastName FROM users WHERE notifyBoard=1 AND email!='' && id=1");
 
 
--- public_html/scripts/updateValues.php
+++ public_html/scripts/updateValues.php
@@ -1,36 +1,39 @@
 <?php
 require "../../includes/init.php";
-	
-	
+
+
+
 switch($_GET['type']) {
 
 
-	case "password":	
+	case "password":
 	if($_POST['pass1']!=$_POST['pass2']) {
-		echo '{"error":"Lösenordet måste vara detsamma i båda rutorna"}';
+		//echo '{"error":"Lösenordet måste vara detsamma i båda rutorna"}';
 	}
 	elseif(empty($_POST['pass1'])) {
-		echo '{"error":"Skriv in ett giltigt lösenord i fältet och upprepa samma ord i det andra fältet"}';
+		//echo '{"error":"Skriv in ett giltigt lösenord i fältet och upprepa samma ord i det andra fältet"}';
 	}
 	else {
-		$db->execute("UPDATE users SET password='".$_POST['pass1']."' WHERE id=".$user['id']." LIMIT 1");
-		echo '{"message":"Det nya lösenordet är nu aktivt"}';
+		$db->update("UPDATE users SET password='".$_POST['pass1']."' WHERE id=".$user['id']." LIMIT 1");
+		//echo '{"message":"Det nya lösenordet är nu aktivt"}';
 	}
 	break;
-	
+
 	case "notifications":
 	if(!isset($_POST['ego']['notifyBoard'])) $_POST['ego']['notifyBoard']=0;
-	$db->update("users", $_POST['ego'], "id", $_GET['id']);
-	echo '{"message":"Meddelandeinställningar uppdaterade"}';
+	$db->updateArray("users", $_POST['ego'], "id", $user['id']);
+	//echo '{"message":"Meddelandeinställningar uppdaterade"}';
 	break;
-	
-	
+
+
 	case "ego":
-	$db->update("users", $_POST['ego'], "id", $_GET['id']);
-	echo '{"message":"Uppgifterna uppdaterade"}';	
+	$db->updateArray("users", $_POST['ego'], "id", $user['id']);
+	//echo '{"message":"Uppgifterna uppdaterade"}';
 	break;
-	
+
 }
+
+header("Location: /index.php?page=profile");
 
 
 
